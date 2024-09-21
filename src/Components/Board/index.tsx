5b4983e1e9fb93392c9d72119cc18a28a2ac9007
Paras Kumar <paraskumar4321@gmail.com>
2024-09-21 16:14:59 +0530

    snakeLadder multipler changes

---
+++ src/Components/Board/index.tsx
@@ -0,0 +1,120 @@
+import React, { FC, useCallback, useEffect, useMemo, useState } from "react";
+import Box from "../Box";
+import styles from "./board.module.css";
+interface props {
+  noOfPlayers: number;
+}
+const Board: FC<props> = ({ noOfPlayers }) => {
+  const playersList : Array<any> = useMemo(() => {
+    return [
+      { name: "player1", value: 1, color: "yellow" },
+      { name: "player2", value: 1, color: "red" },
+      { name: "player3", value: 1, color: "blue" },
+      { name: "player4", value: 1, color: "violet" },
+      { name: "player5", value: 1, color: "plum" },
+    ];
+  }, []);
+  const [players, setPlayers] = useState(playersList.slice(0,noOfPlayers));
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+  const [buttonVal, setButtonVal] = useState<string>("Roll Dice");
+  const boxes = useMemo(() => {
+    const arr = Array.from({ length: 10 }, (_, i) =>
+      Array.from({ length: 10 }, (_, j) => i * 10 + j + 1)
+    );
+    return arr;
+  }, []);
+  const getRandomValue = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const snakeBites = useMemo(() => {
+    const points = new Map();
+    while (points.size < 8) {
+      const start = getRandomValue(10, 99);
+      const end = getRandomValue(1, start);
+      points.set(start, end);
+    }
+    return points;
+  }, []);
+
+  const ladderPoints = useMemo(() => {
+    const points = new Map();
+    while (points.size < 8) {
+      const start = getRandomValue(10, 85);
+      const end = getRandomValue(start, 99);
+      points.set(start, end);
+    }
+    return points;
+  }, []);
+  const stepper = useCallback((current: number, nextval: number) => {
+    const intervalId = setInterval(() => {
+      if (current !== nextval) {
+        current > nextval ? current-- : current++;
+        const list = [...players];
+        list[currentPlayer].value = current;
+        setPlayers(list);   
+      } else {
+        clearInterval(intervalId); // Stop once it reaches nextval
+        const ladder = ladderPoints.get(nextval);
+        if (ladder) {
+          stepper(current, ladder);
+        }
+        const didSnakeBite = snakeBites.get(nextval);
+        if (didSnakeBite) {
+          stepper(current, didSnakeBite);
+        }
+        setButtonVal("Roll Dice");
+        setCurrentPlayer((currentPlayer) =>currentPlayer == noOfPlayers-1 ? 0 : currentPlayer+1)
+      }
+    }, 500);
+  }, [currentPlayer]);
+  const rollDice = () => {
+    const number = getRandomValue(1, 6);
+    setButtonVal(number.toString());
+    if(players[currentPlayer].value == 1 && number !== 6){
+      setTimeout(()=>{setButtonVal("Roll Dice")
+      setCurrentPlayer((currentPlayer) =>currentPlayer == noOfPlayers-1 ? 0 : currentPlayer+1)},1000)
+      return;
+    }
+    let current = players[currentPlayer].value;
+    let nextval = current + number;
+    stepper(current, nextval);
+  };
+  const buttonDisabled = useMemo(
+    () => !players || Object.keys(players).length == 0 || buttonVal !== 'Roll Dice',
+    [players,buttonVal]
+  );
+  return (
+    <>
+      <div className={styles.board}>
+       
+        <table>
+          {boxes.map((row1, index) => (
+            <tr key={index}>
+              {row1.map((val, index) => (
+                <Box
+                  active={val == players[currentPlayer].value}
+                  snakeVal={snakeBites.get(val)}
+                  ladderVal={ladderPoints.get(val)}
+                  key={val}
+                  players={players}
+                  value={val}
+                />
+              ))}
+            </tr>
+          ))}
+        </table>
+      </div>
+       <div className={styles.currentPlayer}>currentPlayer : {players[currentPlayer].name}</div>
+      <button
+        disabled={buttonDisabled}
+        onClick={rollDice}
+        className={styles.dice}
+      >
+        {buttonVal}
+      </button>
+    </>
+  );
+};
+
+export default Board;
---
+++ src/Components/Box/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC, useMemo } from "react";
+import styles from "./box.module.scss";
+import classNames from "classnames";
+interface boxProps {
+  value: number;
+  snakeVal?: number;
+  ladderVal?: number;
+  active?: boolean;
+  players : Array<{name:string,value:number,color:string}>
+}
+const Box: FC<boxProps> = ({ value, active, snakeVal, ladderVal ,players}) => {
+  const objForColors = useMemo(()=>{
+    const obj: { [key: number]: string } = {};  // Define the type
+      players.forEach((val,index)=> obj[val.value] = val.color )
+      return obj
+  },[players])
+  return (
+    <div
+      className={classNames([
+        styles.box,
+        active ? styles.active : {},
+        snakeVal ? styles.snakeClass : ladderVal ? styles.ladderClass : {},
+      ])}
+      style={objForColors[value] ? {borderRadius:'100%',backgroundColor:objForColors[value],borderColor:objForColors[value]} : {}}
+    >
+      {(snakeVal || ladderVal) && (
+        <span className={styles.stepper}> {snakeVal || ladderVal}</span>
+      )}
+      <span className={styles.value}>{value}</span>
+    </div>
+  );
+};
+
+export default Box;
